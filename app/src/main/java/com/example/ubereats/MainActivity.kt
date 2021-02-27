d3ec22d72e753e820ad7d7566876e86623684454
VicAnto <A01339529@itesm.mx>
2021-02-27 11:33:31 -0600

    App, terminada
    Falta el dise{o

--- app/src/main/java/com/example/ubereats/MainActivity.kt
+++ app/src/main/java/com/example/ubereats/MainActivity.kt
@@ -2,72 +2,92 @@ package com.example.ubereats
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.Validators.not
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var subtotal = 200.0
+    var subtotal = 0.0
     var compras = mutableListOf<Compras>()
     var orderFee = 0.0
     var serviceFee = 0.0
     var deliveryFee = 0.0
     var tip = 0.0
-    var id = 1
+    var id = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //subtotal = precioUsuario.text.toString().toDouble()
-        orderFee = subtotal*0.02
-        serviceFee = subtotal*0.05
-        deliveryFee = subtotal*0.1
-        textView10Id.text = "$" + (subtotal*0.1).toDouble().toString()
-        textView15Id.text = "$" + (subtotal*0.15).toDouble().toString()
-        textView20Id.text = "$" + (subtotal*0.2).toDouble().toString()
-        textView25Id.text = "$" + (subtotal*0.25).toDouble().toString()
-        OrderFeeId.text = "$" + orderFee.toDouble().toString()
-        ServiceFeeId.text = "$" + serviceFee.toDouble().toString()
-        DeliveryFeeId.text = "$" + deliveryFee.toDouble().toString()
-        calcTotal(orderFee,serviceFee,deliveryFee,tip)
-
-        button10Id.setOnClickListener(){
-            tip = subtotal*0.1
-            calcTotal(orderFee,serviceFee,deliveryFee,tip)
-        }
-        button15Id.setOnClickListener(){
-            tip = subtotal*0.15
-            calcTotal(orderFee,serviceFee,deliveryFee,tip)
-        }
-        button20Id.setOnClickListener(){
-            tip = subtotal*0.2
-            calcTotal(orderFee,serviceFee,deliveryFee,tip)
-        }
-        button25Id.setOnClickListener(){
-            tip = subtotal*0.25
-            calcTotal(orderFee,serviceFee,deliveryFee,tip)
-        }
-        button0Id.setOnClickListener(){
-            tip = subtotal*0.0
-            calcTotal(orderFee,serviceFee,deliveryFee,tip)
-        }
-
+        init()
     }
+
+    fun init(){
+        precioUsuario.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(!TextUtils.isEmpty(precioUsuario.text.toString())){
+                    subtotal = precioUsuario.text.toString().toDouble()
+                    orderFee = subtotal*0.02
+                    serviceFee = subtotal*0.05
+                    deliveryFee = subtotal*0.1
+                    OrderFeeId.text = "$" + orderFee.toDouble().toString()
+                    ServiceFeeId.text = "$" + serviceFee.toDouble().toString()
+                    DeliveryFeeId.text = "$" + deliveryFee.toDouble().toString()
+                    calcTotal(orderFee,serviceFee,deliveryFee,tip)
+
+                    button10Id.setOnClickListener(){
+                        tip = subtotal*0.1
+                        calcTotal(orderFee,serviceFee,deliveryFee,tip)
+                    }
+                    button15Id.setOnClickListener(){
+                        tip = subtotal*0.15
+                        calcTotal(orderFee,serviceFee,deliveryFee,tip)
+                    }
+                    button20Id.setOnClickListener(){
+                        tip = subtotal*0.2
+                        calcTotal(orderFee,serviceFee,deliveryFee,tip)
+                    }
+                    button25Id.setOnClickListener(){
+                        tip = subtotal*0.25
+                        calcTotal(orderFee,serviceFee,deliveryFee,tip)
+                    }
+                    button0Id.setOnClickListener(){
+                        tip = 0.0
+                        calcTotal(orderFee,serviceFee,deliveryFee,tip)
+                    }
+                }
+            }
+        })
+    }
+
     fun calcTotal(orderFee: Double, serviceFee:Double, deliveryFee:Double,tip:Double): Double {
-        var total = subtotal + orderFee + serviceFee +deliveryFee + tip
+        var total = subtotal + orderFee + serviceFee + deliveryFee + tip
         compraButon.text = "Pagar $" + total.toString()
         return total
     }
-
     fun guarda(view: View){
-        compraButon.setOnClickListener(){
-            id += 1
-            ordenId.text = "Orden # " + id.toString()
-        }
+        id += 1
+        ordenId.text = "Orden # " + id.toString()
         var total = calcTotal(orderFee,serviceFee,deliveryFee,tip)
         val compra = Compras(id, subtotal, orderFee, serviceFee, deliveryFee, total)
         compras.add(compra)
+        precioUsuario.text.clear()
+        OrderFeeId.text = "$0.0"
+        ServiceFeeId.text = "$0.0"
+        DeliveryFeeId.text = "$0.0"
+        compraButon.text = "Pagar"
+        subtotal = 0.0
+        orderFee = 0.0
+        serviceFee = 0.0
+        deliveryFee = 0.0
         Log.i("com.example.ubereats.Hola", compras.toString())
     }
 
