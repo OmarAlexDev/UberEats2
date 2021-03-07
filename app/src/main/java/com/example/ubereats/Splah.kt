f7eaea46dc5ac9b999975b0ff0cf299311aebb47
VicAnto <A01339529@itesm.mx>
2021-03-07 16:12:44 -0600

    Pantalla de carga terminada

---
+++ app/src/main/java/com/example/ubereats/Splah.kt
@@ -0,0 +1,24 @@
+package com.example.ubereats
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class Splah : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splah)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler().postDelayed({
+            val intento = Intent(this, MainActivity::class.java)
+            startActivity(intento)
+            finish()
+        }, 3000)
+    }
+
+}
